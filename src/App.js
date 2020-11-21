f8c4e8900bbddd0758b175aba2bb1fe71efffe6a
mike <mikesapienzaaudio@gmail.com>
2020-11-21 15:08:00 -0600

    labs pass but couldn"t render names in ptag

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(astros => this.setState({astrosResp: astros}))
+    }
+
+    renderAstros = () => {
+        debugger
+        // console.log(this.state.astrosResp)
+        // this.state.astrosResp.people.map(person => {
+        //    return <p>{person.name}</p>
+        // })
+    }
+
+    render() { 
+        return ( 
+            <div>
+                {this.renderAstros}
+            </div>
+         );
+    }
+}
+ 
+export default App;
